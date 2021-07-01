e922ae1f3638b0fda041c7cacaacebc92de54702
joerg <jmdev@online.ms>
2021-07-01 17:15:16 +0200

    fix json load issue by recompile

--- APM/apm-new/src/app/products/product.service.ts
+++ APM/apm-new/src/app/products/product.service.ts
@@ -9,12 +9,13 @@ import { IProduct } from "./product";
 })
 export class ProductService {
   
-  private productUrl = 'assets/products/products.json';
+  private productUrl = 'api/products/products.json';
   constructor(private http: HttpClient){ }
   
   getProducts(): Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(this.productUrl).pipe(
-            tap(data => console.log('All',JSON.stringify(data))),
+        return this.http.get<IProduct[]>(this.productUrl)
+        .pipe(
+          tap(data => console.log('All: ', JSON.stringify(data))),
             catchError(this.handleError)
         );
     }
